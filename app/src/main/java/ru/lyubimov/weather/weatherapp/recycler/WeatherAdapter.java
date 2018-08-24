9484dbb8efc90f99a806d1857f9657543c37c4fa
Alexandr Lyubimov <dr_kuvento@mail.ru>
2018-08-25 01:48:41 +0300

    Пункты 3,5 домашнего задания.

--- app/src/main/java/ru/lyubimov/weather/weatherapp/recycler/WeatherAdapter.java
+++ app/src/main/java/ru/lyubimov/weather/weatherapp/recycler/WeatherAdapter.java
@@ -2,38 +2,145 @@ package ru.lyubimov.weather.weatherapp.recycler;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ActionMode;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import ru.lyubimov.weather.weatherapp.R;
+import ru.lyubimov.weather.weatherapp.ViewUtils;
 import ru.lyubimov.weather.weatherapp.model.Weather;
 
-public class WeatherAdapter extends RecyclerView.Adapter<WeatherHolder> {
-        private List<Weather> mWeathers;
-        private Activity mActivity;
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherHolder> {
 
-        public WeatherAdapter(List<Weather> weathers, Activity activity) {
-            mWeathers = weathers;
-            mActivity = activity;
-        }
+    private List<Weather> weathers;
+    private Activity activity;
+    private boolean multiSelect = false;
+    private List<Weather> selectedItems = new ArrayList<>();
+    private ActionMode actionMode;
 
-        @NonNull
+    private ActionMode.Callback actionModeCallbacks = new ActionMode.Callback() {
         @Override
-        public WeatherHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(mActivity);
-            return new WeatherHolder(inflater, parent, mActivity.getApplicationContext());
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull WeatherHolder holder, int position) {
-            Weather weather = mWeathers.get(position);
-            holder.bind(weather);
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            multiSelect = true;
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.context_menu, menu);
+            actionMode = mode;
+            return true;
         }
 
         @Override
-        public int getItemCount() {
-            return mWeathers.size();
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
         }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            for (Weather intItem : selectedItems) {
+                weathers.remove(intItem);
+            }
+            mode.finish();
+            return true;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            multiSelect = false;
+            selectedItems.clear();
+            notifyDataSetChanged();
+        }
+    };
+
+    public WeatherAdapter(List<Weather> weathers, Activity activity) {
+        this.weathers = weathers;
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public WeatherHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(activity);
+        return new WeatherHolder(inflater, parent);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherHolder holder, int position) {
+        Weather weather = weathers.get(position);
+        holder.bind(weather);
+    }
+
+    @Override
+    public int getItemCount() {
+        return weathers.size();
+    }
+
+    class WeatherHolder extends RecyclerView.ViewHolder {
+        private Weather mWeather;
+
+        private TextView mDateInfo;
+        private ImageView mWeatherInfo;
+        private TextView mTempInfo;
+        private ConstraintLayout mLayout;
+
+        WeatherHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.time_stamp_item, parent, false));
+            mDateInfo = itemView.findViewById(R.id.date_info);
+            mWeatherInfo = itemView.findViewById(R.id.weather_ico);
+            mTempInfo = itemView.findViewById(R.id.temp_text);
+            mLayout = itemView.findViewById(R.id.layout);
+        }
+
+        void bind(final Weather weather) {
+            mWeather = weather;
+            ViewUtils.setTimeStamp(activity.getResources(), mDateInfo, mWeather.getDateStamp());
+            ViewUtils.setWeatherIcon(activity.getApplicationContext(), mWeatherInfo, mWeather.getCondition().getIconName());
+            ViewUtils.setTemperatureInformation(activity.getResources(), mTempInfo, mWeather.getTemperature());
+
+            if (selectedItems.contains(weather)) {
+                mLayout.setBackgroundColor(activity.getResources().getColor(R.color.colorRowSelected));
+            } else {
+                mLayout.setBackgroundColor(activity.getResources().getColor(R.color.colorBackgroundDay));
+            }
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    ((AppCompatActivity) view.getContext()).startSupportActionMode(actionModeCallbacks);
+                    selectItem(weather);
+                    return true;
+                }
+            });
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    selectItem(weather);
+                }
+            });
+        }
+
+        void selectItem(Weather weather) {
+            if (multiSelect) {
+                if (selectedItems.contains(weather)) {
+                    selectedItems.remove(weather);
+                    mLayout.setBackgroundColor(activity.getResources().getColor(R.color.colorBackgroundDay));
+                    if(selectedItems.isEmpty()) actionMode.finish();
+                } else {
+                    selectedItems.add(weather);
+                    mLayout.setBackgroundColor(activity.getResources().getColor(R.color.colorRowSelected));
+                }
+            }
+        }
+    }
 }
