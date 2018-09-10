4c62abe79e83fb40be2b1b188c1e00f5eb622c1f
Alexandr Lyubimov <dr_kuvento@mail.ru>
2018-09-10 22:08:01 +0300

    Урок 5. ДЗ. Откорректрировал сообщение, убрал лишние импорты.

--- app/src/main/java/ru/lyubimov/weather/weatherapp/data/weather/network/retrofit/OpenWeatherMapRetroFetcher.java
+++ app/src/main/java/ru/lyubimov/weather/weatherapp/data/weather/network/retrofit/OpenWeatherMapRetroFetcher.java
@@ -6,8 +6,6 @@ import android.util.Log;
 import java.util.Locale;
 
 import io.reactivex.Single;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
